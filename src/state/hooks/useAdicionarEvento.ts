f561b20d3ca75a4ebec53761254b3df208644085
Miler <estudos.miler@gmail.com>
2024-05-09 18:31:55 -0300

    first commit

---
+++ src/state/hooks/useAdicionarEvento.ts
@@ -0,0 +1,20 @@
+import { useSetRecoilState } from "recoil"
+import { listaDeEventosState } from "../atom"
+import { IEvento } from "../../interfaces/IEvento"
+import { obterId } from "../../util"
+
+
+const useAdicionarEvento = () => {
+    const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+    
+    return(evento: IEvento) =>{
+        const hoje = new Date()
+        if(evento.inicio < hoje){
+            throw new Error('Eventos não podem ser criados com data retroativa!')
+        }
+        evento.id = obterId()
+        return setListaDeEventos(listaAntiga => [...listaAntiga, evento])
+    }
+}
+
+export default useAdicionarEvento
---
+++ src/state/hooks/useListaDeEventos.ts
@@ -0,0 +1,10 @@
+import { useRecoilValue } from "recoil"
+
+import { eventosFiltradosState } from "../seletores"
+
+
+const useListaDeEventos = () => {
+    return useRecoilValue(eventosFiltradosState)
+}
+
+export default useListaDeEventos
